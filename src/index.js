848cb2b60be6508d84c3f838f3410650473c0d10
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-02-26 10:58:32 +1100

    Creating an action Creator

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import store from "./store";
 import * as actions from "./actionTypes";
+import { bugAdded } from "./actions";
 const unsubscribe = store.subscribe(() => {
   console.log("Store Changed", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug 1",
-  },
-});
+store.dispatch(bugAdded("Bug 2"));
 
 unsubscribe();
 store.dispatch({
