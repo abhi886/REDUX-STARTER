75f6a30587a282fcd3f817d1870d2c2be89c4a18
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-02-25 18:39:34 +1100

    Dispatch an Action

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import store from "./store";
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug 1",
+  },
+});
+
+console.log(store.getState());
