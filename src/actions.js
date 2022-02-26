c6380214052b2551e00d2e4d3a60fdf03a10f260
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-02-26 11:48:40 +1100

    Excercise and solution

--- src/actions.js
+++ src/actions.js
@@ -6,11 +6,10 @@ export const bugAdded = (description) => ({
     description,
   },
 });
-// export function bugAdded(description) {
-//   return {
-//     type: actions.BUG_ADDED,
-//     payload: {
-//       description,
-//     },
-//   };
-// }
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id,
+  },
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import store from "./store";
 import * as actions from "./actionTypes";
-import { bugAdded } from "./actions";
+import { bugAdded, bugResolved } from "./actions";
 const unsubscribe = store.subscribe(() => {
   console.log("Store Changed", store.getState());
 });
 
+store.dispatch(bugAdded("Bug 1"));
+
 store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugResolved(1));
+store.dispatch(bugResolved(3));
 
 unsubscribe();
 store.dispatch({
